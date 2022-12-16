a337d75bb4d120a2366864790007c8db3636224a
Ana Carolina Aquino <113844035+AnaCarolinaAquino@users.noreply.github.com>
2022-12-16 06:57:32 -0300

    innitial commit

---
+++ REACT ATUALIZADO/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FooterStyled } from './styles'
+
+function Footer() {
+    return (
+        <FooterStyled>
+            DupladeDois 2022 - Alguns direitos reservados
+        </FooterStyled>
+    )
+}
+
+export default Footer;
